e81f13c2610f1f9bb4e84ec65e171d3115b13a5b
Henno Täht <henno.taht@khk.ee>
2015-09-02 14:56:00 +0300

    Esialgse lehe ülespanek [Delivers #102571416]

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="et">
+<head>
+    <meta charset="UTF-8">
+    <title>Project1</title>
+</head>
+<body>
+<h1>Tere maailm!</h1>
+<hr>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam aperiam, aspernatur at deserunt doloribus
+    id labore maxime nam obcaecati officia provident, repellat reprehenderit soluta sunt, tempora temporibus vitae
+    voluptatum. Alias consectetur consequatur delectus deleniti dignissimos distinctio dolorum eum expedita fugiat illo
+    ipsum iusto labore minus modi, nulla officiis omnis provident quasi quibusdam, quis, quod repudiandae similique sit
+    tempore vero. Alias blanditiis culpa dignissimos doloremque dolores est explicabo iste laborum modi natus nihil
+    obcaecati odio qui, quo quos unde vitae! Alias aliquam aspernatur beatae consequatur debitis, deleniti deserunt
+    dolor dolores, hic minima officia quaerat saepe sit tempore voluptate. Aliquid, architecto aspernatur, debitis
+    dolores eius eos ipsum itaque nam perferendis praesentium quos, rem repudiandae. Adipisci cupiditate delectus
+    distinctio dolor dolorum ducimus exercitationem fugiat in ipsum odio, odit quae quis quisquam rem, sequi vitae
+    voluptates! Excepturi, incidunt mollitia? Ab accusamus culpa eaque earum illo mollitia possimus rem, totam. A ab ad
+    adipisci aspernatur at aut culpa cum deleniti distinctio dolorum error eum eveniet ex expedita explicabo fugiat hic,
+    illo impedit itaque laborum minus mollitia nisi numquam omnis placeat possimus quaerat quasi quibusdam quisquam
+    recusandae saepe sequi tempore tenetur veritatis vero voluptates voluptatum! Accusantium aliquam asperiores
+    aspernatur atque consectetur cupiditate delectus doloribus eligendi excepturi exercitationem hic id ipsum laboriosam
+    libero natus neque numquam quasi quia quis quod rem saepe sit sunt ullam unde voluptas voluptatibus, voluptatum? At
+    consequuntur dignissimos dolorem ducimus ea eligendi explicabo facere incidunt ipsam itaque labore maiores minima
+    nemo nisi nobis, omnis pariatur perspiciatis possimus provident, recusandae reprehenderit tempora temporibus
+    veritatis. Accusantium ad amet blanditiis consequuntur culpa dolorum ea eius enim excepturi exercitationem facilis,
+    fugit hic inventore iusto laborum maiores minus, natus necessitatibus nemo nihil nostrum optio placeat porro
+    possimus quae rerum, sequi velit? Est incidunt laborum quod. Alias culpa est explicabo facere hic iste libero magni
+    maxime repudiandae vero!</p>
+</body>
+</html>
